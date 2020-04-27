91e443adb36297857e5aff0d217a62b7060bd96e
wangjchs <wangjchs@yonyou.com>
2020-04-27 16:25:56 +0800

    ':beers:'

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,43 @@
 const webpack = require('webpack')
 const path = require('path')
+const glob = require('glob')
 const HtmlwebpackPlugin = require('html-webpack-plugin')
+
+function getFileCollection() {
+  const globPath = './src/**/*.*(jsx|js)'
+  const files = glob.sync(globPath)
+  return files
+}
+
+function entryConfig() {
+  let entryObj = {}
+  getFileCollection().forEach(item => {
+    const filePath = item.replace('./src', '')
+    entryObj[filePath] = path.resolve(__dirname, item)
+  })
+  return entryObj
+}
 
 module.exports = function() {
   return {
-    entry: ['./src/index.js'],
+    entry: entryConfig(),
     output: {
-      path: path.resolve(__dirname, 'build'),
-      filename: 'static/js/bundle.js'
+      filename: chunkData => {
+        let filePath = chunkData.chunk.name
+        const filename = filePath.replace('.jsx', '.js')
+        return filename
+      },
+      path: __dirname + '/dist',
+      libraryTarget: 'umd'
     },
+    externals: [
+      function(context, request, callback) {
+        if (/.js|.jpg|.png|.gif|.svg|.jpeg$/g.test(request)) {
+          return callback()
+        }
+        callback(null, request)
+      }
+    ],
     module: {
       rules: [
         {
